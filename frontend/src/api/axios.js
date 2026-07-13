de46f2c5a1a15b5bea3d7618f8bb64d3127dabac
Sabab <sbalex2357@gmail.com>
2026-07-13 12:06:15 +0600

    api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL:"http://localhost:5000",
-    baseURL:"https://project06-u2qf.vercel.app",
+    baseURL:"https://project06-epjw.vercel.app",
     withCredentials:true
 })
 
