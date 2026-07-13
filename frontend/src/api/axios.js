f2776c0c66341393ed8fa511c7eb4576b4171d36
Sabab <sbalex2357@gmail.com>
2026-07-13 14:16:51 +0600

    add

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL:"http://localhost:5000",
-    baseURL:"https://backend-production-bd60.up.railway.app/",
+    baseURL:"https://backend-production-bd60.up.railway.app",
     withCredentials:true
 })
 
