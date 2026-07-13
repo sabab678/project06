6ee1a6b61e2051c0fd9bc50586faef0cf65c2c57
Sabab <sbalex2357@gmail.com>
2026-07-13 14:11:56 +0600

    push

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL:"http://localhost:5000",
-    baseURL:"https://project06-epjw.vercel.app",
+    baseURL:"https://backend-production-bd60.up.railway.app/",
     withCredentials:true
 })
 
